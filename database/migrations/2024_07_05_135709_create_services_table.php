0ae135e24845508e1c4e3862a22da9fe4b27ff49
Miloud Ajouamy <ajouamymiloud@gmail.com>
2024-07-11 20:23:06 +0100

    making models migrations , controllers

--- database/migrations/2024_07_05_135709_create_services_table.php
+++ database/migrations/2024_07_05_135709_create_services_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('works', function (Blueprint $table) {
+        Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('works');
+        Schema::dropIfExists('services');
     }
 };
